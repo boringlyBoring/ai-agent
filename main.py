ad991252a41b244c9d7d4d9da68a61b34bc111f6
Mohd Rameez <rameezmohd739@gmail.com>
2025-09-12 00:06:16 +0530

    Add arguments to cli and add a calculator app

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key = api_key)
+def main():
 
-response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-print(response.text)
+    client = genai.Client(api_key = api_key)
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if len(sys.argv) < 2:
+        print("Please provide a prompt in arguments")
+        sys.exit(1)
+
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == '--verbose':
+        verbose_flag = True
+
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role = 'user', parts = [types.Part(text=prompt)])
+    ]
+
+    response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents = messages
+    )
+
+    print(response.text)
+    
+    if verbose_flag:    
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
